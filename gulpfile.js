a12ba2430a52eab165209aef226ec2bbea4d1b51
Luciano Baraúna <baraunaluciano@gmail.com>
2018-07-17 16:55:20 -0300

    acertando tarefa de proessamento do pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,37 @@
 const gulp = require('gulp')
-const gulpPug = require('gulp-pug')
-const gulpClean = require('gulp-clean')
-const gulpPlumber = require('gulp-plumber')
+const pug = require('gulp-pug')
+const clean = require('gulp-clean')
+const plumber = require('gulp-plumber')
+const browserSync = require('browser-sync')
 
 gulp.task('task:clean', () => {
    return gulp.src('email')
-    .pipe(gulpClean()) 
+    .pipe(clean()) 
 })
 
 gulp.task('task:pug', () => {
-    return gulp.src('src/pug/template/*.pug')
-        .pipe(gulpPlumber())
-        .pipe(gulpPug({
+    return gulp.src('./src/pug/template/*.pug')
+        .pipe(plumber())
+        .pipe(pug({
             pretty: '\t',
             compileDebug: true
         }))
-        .pipe(gulp.dest('./email/'))
+        .pipe(gulp.dest('./src/html'))
+        .pipe(browserSync.stream({match: 'pug/**/*.pug'}));
 })
+
+gulp.task('task:pug-watch', ['task:pug'], (done) => {
+    browserSync.reload();
+    done();
+});
+
+
+// Static server
+gulp.task('task:server', () => {
+    browserSync.init({
+        server: {
+            baseDir: "./src/html/"
+        }
+    });
+    gulp.watch("./src/pug/template/*.pug", ['task:pug-watch']);
+});
