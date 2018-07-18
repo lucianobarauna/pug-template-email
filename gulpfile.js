1157deefc5caf6698c666a303aa00ef53830606f
Luciano Baraúna <baraunaluciano@gmail.com>
2018-07-17 23:04:53 -0300

    finalizando tarefas do gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,41 +3,43 @@ const pug = require('gulp-pug')
 const clean = require('gulp-clean')
 const plumber = require('gulp-plumber')
 const browserSync = require('browser-sync')
+const imagemin = require('gulp-imagemin')
 
 gulp.task('task:clean', () => {
-   return gulp.src('email')
-    .pipe(clean()) 
+  return gulp.src('email')
+  .pipe(clean())
 })
 
 gulp.task('task:pug', () => {
-    // return gulp.src('./src/pug/template/*.pug')
-    return gulp.src('./src/pug/template/*.pug')
-        .pipe(plumber())
-        .pipe(pug({
-            pretty: '\t',
-            compileDebug: true
-        }))
-        .pipe(gulp.dest('./src/html'))
-
-        .pipe(browserSync.reload({stream: true}))
+  return gulp.src('./src/pug/template/*.pug')
+    .pipe(plumber())
+    .pipe(pug({
+        pretty: '\t',
+        compileDebug: true
+    }))
+    .pipe(gulp.dest('./email/'))
+    .pipe(browserSync.reload({stream: true}))
 })
 
-
-gulp.task('reloadTeste', ['task:pug'], function (done) {
-    setTimeout(() => {
-        browserSync.reload();
-    done();
-    }, 500)
-    
-        
-});
+gulp.task('task:img', () => {
+  return gulp.src('./src/img/*.jpg')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./email/'))
+})
 
 // Static server
 gulp.task('task:server', () => {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    gulp.watch("./src/pug/template/*.pug", ['reloadTeste']);
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+  gulp.watch("src/pug/template/*.pug", ['task:pug']);
+  gulp.watch("src/img/*.jpg", ['task:img']);
+  gulp.watch("email/*.html").on("change", browserSync.reload);
 });
+
+gulp.task("task:build", ['task:clean'], () => {
+  gulp.start('task:pug', 'task:img', 'task:server')
+})
+
