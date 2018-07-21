b3d45c05fd4e60122d9977dfd33e632dbe6aa7bf
Luciano Baraúna <baraunaluciano@gmail.com>
2018-07-21 19:49:03 -0300

    tentando resolver import do data

--- gulpfile.js
+++ gulpfile.js
@@ -4,32 +4,37 @@ const clean = require('gulp-clean')
 const plumber = require('gulp-plumber')
 const browserSync = require('browser-sync')
 const imagemin = require('gulp-imagemin')
+const fs = require('fs');
 
+// Clear folder email
 gulp.task('task:clean', () => {
   return gulp.src('email')
   .pipe(clean())
 })
 
+// Render Pug
 gulp.task('task:pug', () => {
   return gulp.src('./src/pug/template/*.pug')
     .pipe(plumber())
     .pipe(pug({
         pretty: '\t',
         compileDebug: true,
-        data: (file) => {
-          console.log("ola");
-
+        locals(file){
+          return JSON.parse(fs.readFileSync('./data/dataNews.json'))
         }
     }))
     .pipe(gulp.dest('./email/'))
     .pipe(browserSync.reload({stream: true}))
 })
 
+// Copy images to folder email
 gulp.task('task:copyimg', () => {
   return gulp.src('./src/img/*.jpg')
     .pipe(gulp.dest('./email/'))
+    .pipe(browserSync.reload({stream: true}))
 })
 
+// Build image
 gulp.task('task:buildimg', () => {
     return gulp.src('./src/img/*.jpg')
       .pipe(imagemin())
@@ -44,11 +49,14 @@ gulp.task('task:server', () => {
       }
   });
   gulp.watch("src/pug/template/*.pug", ['task:pug']);
-  gulp.watch("src/img/*.jpg", ['task:copyimg']);
+  gulp.watch(["src/img/*.jpg", "email/*.html"], ['task:copyimg']);
   gulp.watch("email/*.html").on("change", browserSync.reload);
 });
 
+
+// Build
 gulp.task("task:build", ['task:clean'], () => {
   gulp.start('task:pug', 'task:buildimg', 'task:server')
 })
+
 
